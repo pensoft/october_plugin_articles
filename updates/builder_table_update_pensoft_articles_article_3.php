65464188b9cf3d24637259de192736e43f9d66af
root <root@project-dev.pensoft.local>
2024-01-03 16:28:24 +0200

    meta keywords

---
+++ updates/builder_table_update_pensoft_articles_article_3.php
@@ -0,0 +1,23 @@
+<?php namespace Pensoft\Articles\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdatePensoftArticlesArticle3 extends Migration
+{
+    public function up()
+    {
+        Schema::table('pensoft_articles_article', function($table)
+        {
+            $table->text('caption')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('pensoft_articles_article', function($table)
+        {
+            $table->dropColumn('caption');
+        });
+    }
+}
